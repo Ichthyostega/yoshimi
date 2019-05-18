0078c671434a68505f5e13d424985569cf5b1455
Ichthyostega <prg@ichthyostega.de>
2019-05-18 13:01:49 +0200

    Restore a configurable calculation buffer size
    this is a partial revert of changes from Will earlier in this "buffer branch"
    > 26.03.19 19:21 abrolag<willgodfrey@musically.me.uk>
    > First attempt at fixed size internal buffer Alsa not done All timings good except LV2 now drops notes.
    
    I consider the actual calculation buffer size to be
    a very important trade-off to be taken by the user and no one else.
    
    The way the whole Yoshimi SynthEngine is written does not allow for
    changing the SynthEngine::buffersize on-the-fly, since various elements
    of sound processing directly base their timing calculations on the relation
    of SynthEngine::buffersize and samplerate *at the time of NoteOn*
    
    This leaves us with the following trade-off:
    * small calc buffer means low latency and fine grained resolution of NoteON times
    * large calc buffer means increased calculation throughput due to better usage
      of the caching and optimisation of modern CPUs, yet the price to pay is
      increased latency and more coarse grained NoteON timings.
    
    NOTE: this change-set alone is incomplete; we need to accommodate at the output side

--- src/MusicIO/JackEngine.cpp
+++ src/MusicIO/JackEngine.cpp
@@ -420,10 +420,10 @@ bool JackEngine::processAudio(jack_nframes_t nframes)
         return false;
     }
 
-    int framesize = sizeof(float) * ActualBufferSize;
-    for (unsigned int pos = 0; pos < nframes; pos += ActualBufferSize)
+    int framesize = sizeof(float) * synth->buffersize;
+    for (unsigned int pos = 0; pos < nframes; pos += synth->buffersize)
     {
-        if (nframes < ActualBufferSize)
+        if (nframes < synth->buffersize)   //////////TODO comparison signed / unsigned; however, this whole "calculate remainder" approach is questionable...
         {
             synth->MasterAudio(zynLeft, zynRight, nframes);
             sendAudio(sizeof(float) * nframes, 0);
@@ -431,7 +431,7 @@ bool JackEngine::processAudio(jack_nframes_t nframes)
         }
         else
         {
-            synth->MasterAudio(zynLeft, zynRight, ActualBufferSize);
+            synth->MasterAudio(zynLeft, zynRight, synth->buffersize);
             sendAudio(framesize, pos);
         }
     }
