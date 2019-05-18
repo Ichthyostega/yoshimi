14bd1e86464580d4fa35b45f2645c528084c3de1
Ichthyostega <prg@ichthyostega.de>
2019-05-18 13:57:29 +0200

    Do not manipulate the buffersize (Alsa)
    The Alsa-Engine used to manipulate the Buffersize in the Configuration,
    when, after opening the interface, the ALSA framework reported back a different
    *output* buffersize than initially requested.
    
    This is altogether unnecessary, since all output frameworks are able to
    deal with an output buffer size different than the *calculation* buffersize
    of SynthEngine.
    
    Config::Buffersize (which is written to XML), SynthEngine::buffersize
    and the output buffersize are three distinct concepts and should be kept apart.

--- src/Misc/Config.cpp
+++ src/Misc/Config.cpp
@@ -921,12 +921,9 @@ void Config::StartupReport(string clientName)
     Log(report, 2);
     if (fullInfo)
     {
+        Log("Buffsize: " + asString(synth->buffersize), 2);
         Log("Oscilsize: " + asString(synth->oscilsize), 2);
         Log("Samplerate: " + asString(synth->samplerate), 2);
-        if (audioEngine == alsa_audio)
-            Log("Period size: " + asString(Buffersize), 2);
-        //else
-            //Log("Period size: " + asString(synth->buffersize), 2);
     }
 }
 //#endif
