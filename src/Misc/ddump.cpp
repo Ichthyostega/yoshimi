8743b9ead98c5262550067b30b68c213fc7f3710
Ichthyostega <prg@ichthyostega.de>
2024-04-13 17:18:22 +0200

    dumplog - new files

---
+++ src/Misc/ddump.cpp
@@ -0,0 +1,30 @@
+/*
+    ddump.h - DEBUG / diagnostics helper for dump-logging
+
+    Copyright 2024,  Ichthyostega
+
+    This file is part of yoshimi, which is free software: you can redistribute
+    it and/or modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the License,
+    or (at your option) any later version.
+
+    yoshimi is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU General Public License (version 2
+    or later) for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with yoshimi.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include "Misc/ddump.h"
+#include "Misc/FormatFuncs.h"
+
+//#include <unordered_map>
+//#include <climits>
+//#include <chrono>
+//#include <atomic>
+//#include <mutex>
+
