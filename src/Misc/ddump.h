b2a7a25321bd537da02c9c432f6619c55a170715
Ichthyostega <prg@ichthyostega.de>
2024-04-13 18:24:01 +0200

    dumplog - define DebugDump stream logger

--- src/Misc/ddump.h
+++ src/Misc/ddump.h
@@ -22,10 +22,63 @@
 #define DEBUG_DDUMP_H
 
 #include "globals.h"
+#include "Misc/FormatFuncs.h"
 //#include <cassert>
 //#include <utility>
 //#include <memory>
+#include <iostream>
+#include <iomanip>
 
+using std::cout;
+using std::endl;
+
+
+/**
+ * Debug logging helper for output to console or file
+ */
+class DebugDump
+{
+    typedef std::ostream&  (*ManipFun)(std::ostream&);
+    typedef std::ios_base& (*FlagsFun)(std::ios_base&);
+
+    std::ostream* sink;
+
+public:
+    DebugDump()
+        : sink(& std::cout)
+        { }
+
+
+    /** forward any value to the stream sink */
+    template<class VAL>
+    DebugDump& operator<<(VAL const& data)
+    {
+        *sink << data;
+        return *this;
+    }
+
+    /** handle stream manipulator, e.g. endl, flush, setw, setfill... */
+    DebugDump& operator<<(ManipFun manipulator)
+    {
+        manipulator(*sink);
+        return *this;
+    }
+
+    /** handle flags on the stream, e.g. setiosflags, resetiosflags... */
+    DebugDump& operator<<(FlagsFun flagSetter)
+    {
+        flagSetter(*sink);
+        return *this;
+    }
+
+    void flush()
+    {
+        std::flush (*sink);
+    }
+};
+
+
+extern DebugDump dDump;
 
 
 #endif /*DEBUG_DDUMP_H*/
